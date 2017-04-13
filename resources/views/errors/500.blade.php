27b102213c4be0e256329ed670493d97191f7a90
plzzzzg <peilunguo@hotmail.com>
2017-04-13 22:38:27 +0800

    修正反馈页面跳转链接

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,6 +1,6 @@
 @extends('app')
 @section('js')
-    <meta http-equiv= "Refresh" content="4;url=http://127.0.0.1:8000/signup">
+    <meta http-equiv= "Refresh" content="4;url=http://scauwlb.top/it/public/signup">
 @section('content')
     <div class="alert alert-danger">
         <h1>抱歉。找不到这个组！</h1>
